58620e5c30756c5ffbcfc0d40c7eb679464664e4
Nikola Novakovic <nikola.novakovic9@gmail.com>
2019-03-16 07:11:29 -0400

    add a test

---
+++ test.py
@@ -0,0 +1,33 @@
+from sklearn.externals import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+import numpy as np
+import re
+
+test_setntance = "I love this"
+
+filename = 'ml_model_final.sav'
+file = open(filename, 'rb')
+loaded_model = joblib.load(file)
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+corpus = []
+pr = PorterStemmer()
+
+for i in range(0, 1):
+    review = re.sub('[^a-zA-Z]', ' ', test_setntance)
+    review = review.lower()
+    review = review.split()
+    review = [pr.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer()
+X = cv.fit_transform(corpus).toarray()
+
+result = loaded_model.predict(X)
+
+print(result)
