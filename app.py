6407f9f2959ada9ac208cda02c9126143669fddd
Nikola Novakovic <nikola.novakovic9@gmail.com>
2020-12-05 16:48:19 -0500

    Feature/add twitter scraping (#3)
    * wip scraping twitter
    
    * wip scraping
    
    * wrap up twitter scraping

--- app.py
+++ app.py
@@ -1,43 +1,73 @@
+import io
+import os
+import base64
 import pickle
+import tweepy
 import numpy as np
+import matplotlib.pyplot as plt
+
 from tensorflow import keras
-from keras.preprocessing.sequence import pad_sequences
+from dotenv import load_dotenv
+from sentiment_model import SentimentModel
 from flask import Flask, request, Response, render_template
 
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+API_KEY_SECRET = os.getenv("API_KEY_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
+
+
+auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
 app = Flask(__name__)
-model = keras.models.load_model('sentiment_model_lstm_v1/')
-MAX_LENGTH = 21
+model_r = keras.models.load_model('sentiment_model_lstm_v1/')
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/single-tweet')
+def tweet():
+    return render_template('tweet.html')
+
 @app.route('/predict',methods=['POST'])
 def predict():
     message = request.form['message']
+    sm = SentimentModel(model_r)
+    results = sm.predict_sentiments([message])
+    print(f'predictions: {results}')
+    return render_template('result.html',prediction = results[0])
 
-    with open('tokenizer.pickle', 'rb') as handle:
-    	tokenizer = pickle.load(handle) 
-        
-    message = tokenizer.texts_to_sequences([message])
-    message = pad_sequences(message, padding='post', maxlen=MAX_LENGTH)
+@app.route('/scrape', methods=['POST', 'GET'])
+def scrape():
+    if request.method == "POST":
+        query = request.form['message']
 
-    predictions = model.predict(message)
+        tweets = tweepy.Cursor(api.search,
+                               q=query,
+                               lang="en").items(500)
+        tweets = [tweet.text for tweet in tweets]
 
-    indice = np.argmax(predictions[0])
-    probability = predictions[0][indice]
-    valid_prediction = probability > 0.8
-    print(f'predictions: {predictions} | indice - {indice} | probability - {probability}')
+        sm = SentimentModel(model_r)
+        results = sm.predict_sentiments(tweets)
 
-    result = "Not sure"
-    
-    if valid_prediction:
-        if indice == 1:
-            result = "Positive"
-        else:
-            result = "Negative"
-    
-    return render_template('result.html',prediction = result)
+
+        labels, counts = np.unique(results, return_counts=True)
+        plt.bar(labels, counts, width=.5, align='center')
+        plt.gca().set_xticks(labels)
+        img = io.BytesIO()
+        plt.savefig(img, format='png')
+        img.seek(0)
+        plot_url = base64.b64encode(img.getvalue()).decode()
+        plot_url = 'data:image/png;base64,{}'.format(plot_url)
+
+        return render_template('result_s.html',distribution = plot_url, query=query)
+
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
